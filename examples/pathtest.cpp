82a3131e1b84ddc81e61af8cfe8ac8c1a6de12fc
Mike Nolan <tesses@tesses.net>
2025-04-19 10:25:04 -0500

    Make path api more understandable, breaks certain code and have makeabsolute and makerelative

---
+++ examples/pathtest.cpp
@@ -0,0 +1,16 @@
+#include "TessesFramework/Filesystem/VFS.hpp"
+#include <iostream>
+#include <string>
+using namespace Tesses::Framework::Filesystem;
+
+
+int main(int argc, char** argv)
+{
+    VFSPath path("C:/home/user/file");
+    VFSPath path2("D:/home/user/file");
+    VFSPath path3("C:/home/user");
+    std::cout << path.MakeRelative(path2).ToString() << std::endl;
+
+    std::cout << (path3 / path.MakeRelative(path2) / "John").CollapseRelativeParents().ToString() << std::endl;
+    return 0;
+}
