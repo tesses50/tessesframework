929dee2b740fd3830c25005bb000a7c24156a670
Mike Nolan <tesses@tesses.net>
2025-11-11 05:18:12 -0600

    Add arch linux

---
+++ Packaging/Linux/build-arch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd /home/build
+mkdir x86_64
+cd x86_64
+cp ../PKGBUILD
+makepkg
+curl --user tesses50:$GITEA_AUTH \
+     --upload-file *.pkg.tar.zst \
+     https://git.tesseslanguage.com/api/packages/tesses50/arch/core
+cd ..
+mkdir powerpc
+cd powerpc
+cp ../PKGBUILD .
+CARCH=powerpc CMAKE_TOOLCHAIN=/opt/cross/ppc/toolchain.cmake makepkg
+curl --user tesses50:$GITEA_AUTH \
+     --upload-file *.pkg.tar.zst \
+     https://git.tesseslanguage.com/api/packages/tesses50/arch/core
