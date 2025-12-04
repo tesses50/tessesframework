c628e0f9603e45423aeb1c174a906e26b3855164
Mike Nolan <tesses@tesses.net>
2025-12-04 15:14:58 -0600

    Fix basic auth

---
+++ examples/tests.cpp
@@ -0,0 +1,13 @@
+#include "TessesFramework/TessesFramework.hpp"
+int main(int argc, char** argv)
+{
+    Tesses::Framework::TF_Init();
+    std::string theEncoded="dXNlcjpwYXNz";
+    Tesses::Framework::Http::ServerContext ctx(nullptr);
+    ctx.requestHeaders.SetValue("Authorization","Basic " + theEncoded);
+    std::string user;
+    std::string pass;
+    std::cout << Tesses::Framework::Http::BasicAuthServer::GetCreds(ctx,user,pass) << std::endl;
+    std::cout << user << std::endl;
+    std::cout << pass << std::endl;
+}
