ccfa5eeeb30df59499665880deb79f88e0b119bb
Mike Nolan <tesses@tesses.net>
2026-05-27 03:26:54 -0500

    Overhaul cmake configuration, add console api, fix http code that caused issues with cgi-bin

---
+++ Packaging/WinToolsIntel/x86.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_C_COMPILER "/usr/bin/i686-w64-mingw32-gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/i686-w64-mingw32-g++")
+set(CMAKE_C_FLAGS "-static-libgcc -static-libstdc++ -static")
+set(CMAKE_CXX_FLAGS "-static-libgcc -static-libstdc++ -static")
+set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++ -static")
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR i686)
---
+++ Packaging/WinToolsIntel/x86_64.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_C_COMPILER "/usr/bin/x86_64-w64-mingw32-gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/x86_64-w64-mingw32-g++")
+set(CMAKE_C_FLAGS "-static-libgcc -static-libstdc++ -static")
+set(CMAKE_CXX_FLAGS "-static-libgcc -static-libstdc++ -static")
+set(CMAKE_EXE_LINKER_FLAGS "-static-libgcc -static-libstdc++ -static")
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
